e1e2da6ed14dfbc2ef7ea0827c6722c5fb36e661
Ilya Sutormin <dyadyajobs@gmail.com>
2022-09-28 22:25:25 +0500

    commit

--- Animals/Controllers/TableViewController.swift
+++ Animals/Controllers/TableViewController.swift
@@ -61,7 +61,6 @@ class TableViewController: UITableViewController {
         return cell
     }
     
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
